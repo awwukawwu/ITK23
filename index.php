ffa38ab36c25b768d1bf83a9b70436a9d46b619c
awwukawwu <99388387+awwukawwu@users.noreply.github.com>
2024-04-26 14:27:20 +0300

    Update index.php
    uuendus

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
   <body>
     <h1> Meie projekti pealeht</h1>
     <p>See leht arvestab ka muudatustega</p>
-<img src=""
+<img src="https://katrinkabun.com/wp-content/uploads/2021/08/Katrin-Kabun-100-villane-suss-6.jpg"
   </body>
 </html>
